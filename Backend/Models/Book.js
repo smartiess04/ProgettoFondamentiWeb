83dcb637867afaea3761acb39cce0cfe0bd1a426
smartiess04 <martycataleta04@gmail.com>
2026-05-29 15:44:31 +0200

    Modello Book

---
+++ Backend/Models/Book.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose")
+
+const bookModel = new mongoose.Schema({
+    titolo: {
+        type: String,
+        required: true
+    } ,
+    autore: {
+        type: String, 
+        required: true
+    },
+    genere: {
+        type: String,
+        required: true
+    },
+    pagineTotali: {
+        type: Number,
+        required: true
+    },
+    editore: {
+        type: String
+    },
+    isbn : {
+        type: String
+    },
+    copertinaURL: {
+        type: String
+    },
+    descrizione: {
+        type: String
+    },
+    linkAcquisto: {
+        type: String
+    }
+
+})
+
+module.exports = mongoose.model("Book" , bookModel)
