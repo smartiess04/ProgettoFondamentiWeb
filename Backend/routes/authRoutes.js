d394825126bdc554122fd21abdf46e21d628608b
smartiess04 <martycataleta04@gmail.com>
2026-06-04 12:54:16 +0200

    Route per l'autenticazione

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,2 +1,14 @@
 const express = require('express');
 const router = express.Router();
+
+// Importo il controller per l'autenticazione
+const AuthController = require ('../controllers/authController');
+
+// Route per la registrazione
+router.post('/register', AuthController.register);
+
+// Route per il login
+router.post('/login', AuthController.login);
+
+// esporto il router
+module.exports = router;
