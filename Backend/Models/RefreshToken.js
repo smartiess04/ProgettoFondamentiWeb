10173e0f68118c50e891566f8b478f2604d7a01b
Giuseppe <giupino.giupino@gmail.com>
2026-06-06 12:28:57 +0200

    fix login

---
+++ Backend/Models/RefreshToken.js
@@ -0,0 +1,28 @@
+const mongoose= require('mongoose');
+
+const refreshTokenModel= new mongoose.Schema({
+    userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User', // Collegamento al modello Utente
+    required: true,
+  },
+  token: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  expiresAt: {
+    type: Date,
+    required: true,
+  },
+  isRevoked: {
+    type: Boolean,
+    default: false,
+  }
+}, { timestamps: true });
+
+// Il documento verrà eliminato automaticamente dal database quando 
+// l'orario corrente supera il valore di 'expiresAt'.
+refreshTokenSchema.index({ expiresAt: 1 }, { expireAfterSeconds: 0 });
+
+module.exports= mongoose.model("RefreshToken",refreshTokenModel)
--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -1,5 +1,8 @@
 const jwt = require('jsonwebtoken');
+const refreshToken= require('../models/RefreshToken');
 const User = require('../models/User');
+const cookieParser= require ('cookie-parser');
+
 
 // Metodo per registrare un nuovo utente
 async function register(req, res) {
@@ -45,41 +48,88 @@ async function login(req, res) {
         const {email, password} = req.body;
 
         //cercare utente a cui corrisponde tale email
-        const User = await User.findOne({ email });
+        const user = await User.findOne({ email });
 
         //se l'utente non esite
-        if (!User){
+        if (!user){
             return res.status(401).json({ message: 'Invalid email or password'}); //scrivo anche or password per evitare eventuale attacco in cui vogliono sapere se quella mail è registrata o meno, e io non glielo dico TIE'
         }
 
+        //fare match della password
+        const passwordCorretta = await User.passwordComparison(password);
 
-    //fare match della password
-    const passwordCorretta = await User.passwordComparison(password);
-
-    if (!passwordCorretta){
-        return res.status(401).json({ message: 'Invalid email or password'});
+        if (!passwordCorretta){
+            return res.status(401).json({ message: 'Invalid email or password'});
     }
 
-    //generare il JWT da legare all'ID dell'utente 
+//TOKEN
 
-    const tokenSessione = jwt.sign({ userId: User._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
-
-    res.status(200).json({
-        token,
-        user: {
-            id: User._id,
-            username: User.username,
-            email: User.email,
-        }
+    const accessToken = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '15m' });
+    const refreshToken= jwt.sign({userId:user._id},process.env.REFRESH_SECRET, {expiresIn: '7d'});
+    
+    //salvo refresh token nel database per un possibile revok al logout
+    const expiresAt= new Date();
+    expiresAt.setDate(expiresAt.getDate() + 7);
+    
+    await refreshToken.create({
+    userId: user._id,
+    token: refreshTokenString,
+    expiresAt: expiresAt
     });
 
-} catch (error){
-    res.status(500).json({ message:'Logging failed'});
+    const cookieOptions= {
+        httpOnly: true,
+        secure: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'strict',
+    }
+
+    //Invio RefreshToken tramite Cookie HTTP-only
+    res.Cookie('jwt_refresh',refreshTokenString,{
+        ...cookieOptions,
+        path: "api/v1/auth/refresh",
+        maxAge: 7*24*60*60*1000  
+    });
+     
+    //Invio AccessToken nel corpo della richiesta
+    res.Cookie('jwt_access',accessToken,{
+        ...cookieOptions,
+        maxAge: 15*60*1000
+    })
+    res.status(200).json({
+            message: "Successfully Logged",
+            user: {
+                id: user._id,
+                username: user.username,
+                email: user.email,
+            }
+        });
+    } catch (error){
+        res.status(500).json({ message:'Logging failed',error});
+    }
 }
+
+async function refresh(req,res){
+    try{
+
+    }catch{
+
+    }
+
+
+}
+
+async function logout(req,res){
+    try{
+        
+
+    }catch{
+
+    }
 
 }
 
 module.exports = {
     register, 
-    login
+    login,
 };
