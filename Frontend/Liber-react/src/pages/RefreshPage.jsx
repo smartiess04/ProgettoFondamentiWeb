0c582a681687cf79df64eafd725e8562f06de0b6
Giuseppe <giupino.giupino@gmail.com>
2026-06-08 21:45:59 +0200

    "refreshpage 2.0"

--- Frontend/Liber-react/src/pages/RefreshPage.jsx
+++ Frontend/Liber-react/src/pages/RefreshPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import {Link} from 'react-router-dom'
 import  '../style/RefreshPage.css'
 import Logo from "../components/Logo"
@@ -7,17 +7,31 @@ import Logo from "../components/Logo"
 export default function RefreshPage(){
     return(
         <>
-        <div className= "sfondo-container"> 
-            <nav className="nav-refresh">
-                <div className="nav-sinistra">
-                    <Logo/>
-                    <span className="titolo-app">Liber</span>
-                </div>
-                <div className="nav-destra">
-                    <button className= "btn-refresh">Login</button>
-                    <button className="btn-refresh">Registrati</button>
-                </div>
-            </nav>
+        <div className="sfondo-container">
+        
+            <header> 
+                <nav className="nav-refresh">
+                    <div className="nav-sinistra">
+                        <Logo/>
+                        <span className="titolo-app">Liber</span>
+                    </div>
+                    <div className="nav-destra">
+                        <button className= "btn-refresh">Login</button>
+                        <button className="btn-refresh">Registrati</button>
+                    </div>
+                </nav>
+            </header>
+
+            <main className='titoli-refresh'>
+                <h2>Le storie uniscono,Liber le connette</h2>
+                <h3>Niente più letture solitarie. Scopri un sito unico in cui tracciare le tue tappe, 
+                    scambiare opinioni autentiche e vivere ogni libro insieme ad una community che ama leggere quanto te...</h3>
+            </main>
+
+            <footer className='footer-refresh'>
+                <p>Corso di Fondamenti Web 25/26 Martina Cataleta, Francesca Teresa Rosa Augello, Giuseppe Galetta</p>
+            </footer>
+            
         </div>
         
         
